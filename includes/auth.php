df2a1a4e8efda84d655c4df26ee0df2e465de9b8
noorharidy19 <noor2206987@miuegypt.edu.eg>
2024-12-22 20:59:02 +0200

    mvc2 (auth)

---
+++ includes/auth.php
@@ -0,0 +1,14 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+} // Start the session
+
+function checkAuthentication($requiredUserType) {
+    // Check if the user is logged in and has the required user type
+    if (!isset($_SESSION['user_id']) || $_SESSION['UserType'] !== $requiredUserType) {
+        // If not authenticated, redirect to the 404 page
+        header("Location: 404.php");
+        exit();
+    }
+}
+?>
